e257edfea77c846647c8f19b9b2af6ad0a6fccef
marianaviro <marianaviro@hotmail.com>
2016-03-15 13:50:16 -0500

    Ya se pueden consultar todas las alertas

---
+++ app/modulos/alertas/alertas.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('myApp.eventos', ['ngRoute'])
+
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/alertas', {
+        templateUrl: 'modulos/alertas/alertas.html',
+        controller: 'AlertasCtrl'
+    });
+}])
+
+.controller('AlertasCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'http://uniandes-satt.herokuapp.com/alertas'
+    }).then(function successCallback(response) {
+        $scope.alertas = response.data;
+    }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+    });
+}]);
